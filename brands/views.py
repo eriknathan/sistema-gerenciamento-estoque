7512776603a6c6540597ed93ee5599735b00afb7
Erik Nathan <77215294+eriknathan@users.noreply.github.com>
2025-10-15 19:14:21 -0300

    adding pagination in brand list

--- brands/views.py
+++ brands/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, DetailView, \
+    UpdateView, DeleteView
 from django.urls import reverse_lazy
 from . import models
 from . import forms
@@ -8,6 +9,7 @@ class BrandListView(ListView):
     model = models.Brand
     template_name = 'brand_list.html'
     context_object_name = 'brands'
+    paginate_by = 10
 
     def get_queryset(self):
         queryset = super().get_queryset()
